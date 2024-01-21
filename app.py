3533022e3f984918f3f98b5a2f108abce12b8ee1
Deep  Das <115420284+das2490@users.noreply.github.com>
2024-01-21 20:54:19 +0530

    first message

---
+++ app.py
@@ -0,0 +1,12 @@
+name = input("Enter ypur name: ")
+
+if len(name) > 10:
+    print("it must be less than 10 character")
+elif len(name) < 3:
+    print("ist must be more than 3 character")
+else:
+    print("name looks good")
+
+
+
+
